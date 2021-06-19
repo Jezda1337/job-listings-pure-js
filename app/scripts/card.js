e4e16e3fb1ee286434f5f4046b77497582b68954
Jezda1337 <megasrbin1@gmail.com>
2021-06-20 01:11:58 +0200

    :fire::bug::fire:

--- app/scripts/card.js
+++ app/scripts/card.js
@@ -1,5 +1,7 @@
 export const card = (data)=>{
     const card = document.createElement('section');
+    const tags = [data.role,data.level,data.language,data.tools];
+    console.log(typeof data.languages);
     card.classList.add('card');
     (data.featured) ? card.classList.add('card-border') : null;
     card.innerHTML = `
@@ -9,15 +11,29 @@ export const card = (data)=>{
                 <span class="${(data.new) ? 'card__tag-new' : 'd-none'}">New</span>
                 <span class="${(data.featured) ? 'card__tag-featured' : 'd-none'}">Featured</span>
             </section>
+            <h2 class="card__position">${data.position}</h2>
+            <section class="card__position-details">
+                <span class="card__position-detail">${data.postedAt}</span>
+                <span class="card__position-detail">${data.role}</span>
+                <span class="card__position-detail">${data.location}</span>
+            </section>
             <figure class="card__company-logo">
                 <img class="card__company-logo__img" src="${data.logo}" alt="company logo" />
             </figure>
         </section>
 
-        <section class="card__job-tags>
+        <section class="card__job-tags">
+            <a class="card__job-tag" href="#">${data.role}</a>
+            <a class="card__job-tag" href="#">${data.level}</a>
 
+            ${data.languages.map(lang=> `<a class="card__job-tag" href="#">${lang}</a>` ).join(' ')}
+            ${data.tools.map(tool => `<a class="card__job-tag" href="#">${tool}</a>`).join(' ')}
         </section>
     `
 
     return card;
 }
+
+
+// role, level, languages, tools
+
